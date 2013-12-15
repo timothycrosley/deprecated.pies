5081734e07497e26834485891d634a7f3ac7ef28
Timothy Crosley <timothy.crosley@gmail.com>
2013-12-14 20:48:09 -0500

    Fix import of ordered dict on python 2.7

--- pies/collections.py
+++ pies/collections.py
@@ -7,4 +7,7 @@ from .version_info import PY2
 if PY2:
     from UserString import *
     from UserList import *
-    from ordereddict import OrderedDict
+
+    import sys
+    if sys.version_info < (2, 7):
+        from ordereddict import OrderedDict
