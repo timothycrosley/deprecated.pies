2a9b5ab7496428c69c056c224edeae5b4b87161c
Timothy Crosley <timothy.crosley@gmail.com>
2013-12-22 15:28:16 -0500

    Add aditional ast cross python version compatibility support

--- pies/ast.py
+++ pies/ast.py
@@ -6,5 +6,23 @@ from .version_info import PY2
 
 if PY2:
     Try = TryExcept
+
+    def argument_names(node):
+        return [isinstance(arg, Name) and arg.id or None for arg in node.args.args]
+
+    def kw_only_argument_names(node):
+        return []
+
+    def kw_only_default_count(node):
+        return 0
 else:
     TryFinally = ()
+
+    def argument_names(node):
+        return [arg.arg for arg in node.args.args]
+
+    def kw_only_argument_names(self):
+        return [arg.arg for arg in node.args.kwonlyargs]
+
+    def kw_only_default_count(self):
+        return sum(1 for n in node.args.kw_defaults if n is not None)
