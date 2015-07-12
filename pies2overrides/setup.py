b713d996484fac9d2a4a2e85bee3073a98cabacf
Timothy Crosley <timothy.crosley@gmail.com>
2015-07-12 00:31:57 -0700

    Update release to 2.6.7

--- pies2overrides/setup.py
+++ pies2overrides/setup.py
@@ -10,12 +10,12 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='pies2overrides',
-      version='2.6.5',
+      version='2.6.7',
       description='Defines override classes that should be included with pies only if running on Python2.',
       author='Timothy Crosley',
       author_email='timothy.crosley@gmail.com',
       url='https://github.com/timothycrosley/pies',
-      download_url='https://github.com/timothycrosley/pies/blob/master/pies2overrides/dist/pies2overrides-2.6.5.tar.gz?raw=true',
+      download_url='https://github.com/timothycrosley/pies/blob/master/pies2overrides/dist/pies2overrides-2.6.7.tar.gz?raw=true',
       license="MIT",
       install_requires=['ipaddress'],
       requires=['ipaddress'],
